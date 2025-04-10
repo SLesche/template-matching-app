c9da3a93f832d0b027dc83036731ba84d6069cc0
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2025-04-10 15:19:03 +0200

    Update return_plot_legend.m

--- app/return_plot_legend.m
+++ app/return_plot_legend.m
@@ -2,12 +2,12 @@ function [legend_text, title_text, subtitle_text] = return_plot_legend(app, appr
     if approach == "maxcor"
         proper_latency = round(latency, 2);
         title_text = {"Individual ERP matched to grand average via minimizing the squared distance", strcat("ERP Number: ", num2str(app.erp_num), " | Bin Number: ", num2str(app.bin_num))};
-        legend_text = ["ERP num", "Matched Grand Average Waveform", strcat("Correlation Fitted ", app.cfg.component_name)];
+        legend_text = ["ERP", "Matched Grand Average Waveform", strcat("Correlation Fitted ", app.cfg.component_name)];
         subtitle_text = {strcat("MAXCOR: Latency = ", num2str(proper_latency)), strcat("MAXCOR: Correlation = ", num2str(fit_cor)), strcat("Stretch Factor = ", num2str(round(b, 2)))};
     elseif approach == "minsq"
         proper_latency = round(latency, 2);
         title_text = {"Individual ERP matched to grand average via minimizing the squared distance", strcat("ERP Number: ", num2str(app.erp_num), " | Bin Number: ", num2str(app.bin_num))};
-        legend_text = ["ERP num", "Matched Grand Average Waveform", strcat("Minsq Fitted ", app.cfg.component_name)];
+        legend_text = ["ERP", "Matched Grand Average Waveform", strcat("Minsq Fitted ", app.cfg.component_name)];
         subtitle_text = {strcat("MINSQ: Latency = ", num2str(proper_latency)), strcat("MINSQ: Correlation = ", num2str(fit_cor)), strcat("Stretch Factor = ", num2str(round(b, 2)))};
     end
 end
