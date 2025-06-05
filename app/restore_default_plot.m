aeaa5c1e97b36a039939d7268a1ac1efaa0d357a
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2025-06-05 11:20:47 +0200

    restore to orgiinal guess

--- app/restore_default_plot.m
+++ app/restore_default_plot.m
@@ -1,6 +1,6 @@
 function restore_default_plot(app, source)
     if ~exist('source', 'var')
-        source = "reviewed";
+        source = "original";
     end
 
     % reload all plots for that erp, set a and b to their
